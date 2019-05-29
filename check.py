c3421cb6c88ce843b6c55e6cd4e6ba7c7f572f29
Joshua Murithi <37470288+myzalius@users.noreply.github.com>
2019-05-29 12:05:56 +0300

    Update check.py

--- check.py
+++ check.py
@@ -1,6 +1,8 @@
 
 import pymongo
-#this can be used for checking wether the database is communicating
+#this can be used for checking wether the database is communicating using pycharm
+
+""" for start ensure your db is working by using the following keyword $sudo service mongod status"""
 url="mongodb://hostcode,port"
 client=pymongo.MongoClient(url)
 Database=client['fullstack']
